f19bfdcfe1f97d915a3cc1e339418be27d5664d2
Connah Fearnley <connah@cmcrm.co.uk>
2024-01-17 15:54:46 +0000

    IGD/4 Fix variable names #time 5

--- resources/views/member.blade.php
+++ resources/views/member.blade.php
@@ -6,7 +6,7 @@ Member Page for {{ $member->first_name }} {{ $member->last_name }}
 
 @section('body')
 <div>This is the member page for: <strong>{{ $member->first_name }} {{ $member->last_name }}</strong></div>
-<div>Member Joined: <strong>{{ $joinedDate ?? '' }}</strong></div>
-<div>Average Score: <strong>{{ $averageScore ?? 0 }}</strong></div>
-<div>Highest Score: <strong>{{ $highestScore ?? 0 }}</strong></div>
+<div>Member Joined: <strong>{{ $joinedDate }}</strong></div>
+<div>Average Score: <strong>{{ $avgScore }}</strong></div>
+<div>Highest Score: <strong>{{ $highScore }}</strong></div>
 @endSection
